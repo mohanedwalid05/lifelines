3707e627fb30f143e625b1d02c26c3cf598d9e8c
Mohanad Walid Ahmad Sayed Ahmad <ma2105787@qu.edu.qa>
2025-01-22 23:23:43 +0300

    final mvp

--- app/components/DonateButton.js
+++ app/components/DonateButton.js
@@ -7,9 +7,9 @@ export default function DonateButton() {
   return (
     <button
       onClick={() => router.push("/donate")}
-      className="fixed bottom-8 right-8 bg-blue-500 hover:bg-blue-600 text-white px-8 py-4 rounded-full shadow-lg text-xl font-bold flex items-center gap-2 transition-transform hover:scale-105"
+      className="fixed bottom-8 mt-10 right-8 bg-blue-500 hover:bg-blue-600 text-white px-8 py-4 rounded-full shadow-lg text-xl font-bold flex items-center gap-2 transition-transform hover:scale-105"
     >
-      <span>❤️</span>
+      {/* <span>❤️</span> */}
       <span>Donate Now</span>
     </button>
   );
